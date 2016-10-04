8cc86cbcf57ed111637a6988a3aceccdaf52d4f4
bobby <bobzz.zone@gmail.com>
2016-10-04 17:31:07 +0000

    doctypedone

---
+++ graceco_tools/graceco_tools/report/sales_by_product_report_summary/sales_by_product_report_summary.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, bobzz.zone@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales By Product Report Summary"] = {
+	"filters": [
+
+	]
+}
---
+++ graceco_tools/graceco_tools/report/sales_variance_report/sales_variance_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, bobzz.zone@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Variance Report"] = {
+	"filters": [
+
+	]
+}
