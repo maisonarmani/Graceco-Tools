e84c4096f453a3a57d858b5e72d551339c709dc5
bobzz-zone <bobzz.zone@gmail.com>
2016-10-06 01:17:59 +0700

    push fix

--- graceco_tools/graceco_tools/report/material_receipt_list_report/material_receipt_list_report.py
+++ graceco_tools/graceco_tools/report/material_receipt_list_report/material_receipt_list_report.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = ["Date:Date:200","Purchase Order:Link/Purchase Order:200","Supplier:Link/Supplier:200",
+	columns, data = ["Date:Date:200","Purchase Receipt:Link/Purchase Receipt:200","Supplier:Link/Supplier:200",
 	"Item:Link/Item:200","Item Name:Data:200","Description:Data:200",
 	"Qty Received:Float:200","Qty Accepted:Float:200","Qty Rejected:Float:200","Amount:Currency:200"], []
 	item=""
@@ -14,4 +14,8 @@ def execute(filters=None):
 		item = """ and poi.item_code = '{}' """.format(filters.get("item"))
 	if filters.get("supplier"):
 		supplier = """ and po.supplier = '{}' """.format(filters.get("supplier"))
+	data = frappe.db.sql("""select po.posting_date,po.name,po.supplier,poi.item_code,poi.item_name,poi.description,poi.received_qty,poi.qty,poi.rejected_qty,poi.amount from `tabPurchase Receipt Item` poi 
+		join `tabPurchase Receipt` po on poi.parent=po.name 
+		where po.docstatus=1 and po.status="Completed" and (po.posting_date between '{}' and '{}') {} {} {} 
+		""".format(filters.get("from"),filters.get("to"),item,customer,territory),as_list=1 )
 	return columns, data
