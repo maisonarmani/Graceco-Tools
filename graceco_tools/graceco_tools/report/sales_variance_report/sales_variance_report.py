afb504e8242c4c58cf0b886afd4fab34f4619939
bobzz-zone <bobzz.zone@gmail.com>
2016-10-06 20:25:29 +0700

    svr change to qty

--- graceco_tools/graceco_tools/report/sales_variance_report/sales_variance_report.py
+++ graceco_tools/graceco_tools/report/sales_variance_report/sales_variance_report.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = ["Item:Link/Item:200","Item Name:Data:200","Total Ordered:Currency:200","Total Delivered:Currency:200","Variance:Currency:200"], []
+	columns, data = ["Item:Link/Item:200","Item Name:Data:200","Total Ordered:Float:200","Total Delivered:Float:200","Variance:Float:200"], []
 	so_item=""
 	dn_item=""
 	so_item_group=""
@@ -17,10 +17,10 @@ def execute(filters=None):
 		so_item_group = """ and soi.item_group = '{}' """.format(filters.get("item_group"))
 		dn_item_group = """ and dni.item_group = '{}' """.format(filters.get("item_group"))
 	data=frappe.db.sql("""select oo.item_code, oo.item_name,oo.amount,dd.amount,(oo.amount-dd.amount) as total
-		from (select soi.item_code, soi.item_name, sum(soi.amount) as amount
+		from (select soi.item_code, soi.item_name, sum(soi.qty) as amount
 		from `tabSales Order Item` soi join `tabSales Order` so on soi.parent=so.name 
 		where so.docstatus=1 and (so.transaction_date between '{0}' and '{1}') {2} {3} group by soi.item_code) as oo
-		join (select dni.item_code, dni.item_name, sum(dni.amount) as amount
+		join (select dni.item_code, dni.item_name, sum(dni.qty) as amount
 		from `tabDelivery Note Item` dni join `tabDelivery Note` dn on dni.parent=dn.name 
 		where dn.docstatus=1 and (dn.posting_date between '{0}' and '{1}') {4} {5} group by dni.item_code) as dd on oo.item_code = dd.item_code
 	"""
