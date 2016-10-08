e95dcbc28b3a5b13e79434bbab536aee78ef9fde
bobzz-zone <bobzz.zone@gmail.com>
2016-10-08 10:39:51 +0700

    report fix

--- graceco_tools/graceco_tools/report/product_delivery_&_distribution_schedule_report/product_delivery_&_distribution_schedule_report.py
+++ graceco_tools/graceco_tools/report/product_delivery_&_distribution_schedule_report/product_delivery_&_distribution_schedule_report.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = ["Delivery Date:Date:200","Customer:Link/Customer:200","Item Name:Data:200","Qty:Float:200","Territory:Link/Territory:200","Sales:Data:200"], []
+	columns, data = ["Delivery Date:Date:200","Customer:Link/Customer:200","Item Code:Link/Item:200","Item Name:Data:200","Qty:Float:200","Territory:Link/Territory:200","Sales:Data:200"], []
 	item=""
 	customer=""
 	territory=""
@@ -18,7 +18,7 @@ def execute(filters=None):
 		territory = """ and so.territory = '{}' """.format(filters.get("territory"))
 	if filters.get("item_group"):
 		item_group = """ and soi.item_group = '{}' """.format(filters.get("item_group"))
-	data = frappe.db.sql("""select so.delivery_date,so.customer,soi.item_name,soi.qty,so.territory,so.sales_partner
+	data = frappe.db.sql("""select so.delivery_date,so.customer,soi.item_code,soi.item_name,soi.qty,so.territory,so.sales_partner
 	from `tabSales Order Item` soi join `tabSales Order` so on soi.parent=so.name 
 	where so.delivery_date = '{}' {} {} {} {} group by soi.name,so.delivery_date,so.customer order by soi.item_name""".format(filters.get("delivery"),item,territory,customer,item_group),as_list=1)
 	return columns, data
--- graceco_tools/graceco_tools/report/production_yield_variance_report/production_yield_variance_report.js
+++ graceco_tools/graceco_tools/report/production_yield_variance_report/production_yield_variance_report.js
@@ -3,6 +3,41 @@
 
 frappe.query_reports["Production Yield Variance Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd":1
+		},
+		{
+			"fieldname":"to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+			"reqd":1
+		},
+		{
+			"fieldname":"shift",
+			"label": __("Shift"),
+			"fieldtype": "Select",
+			"options": "All\nMorning\nAfternoon\nNight",
+			"default":"All",
+			"reqd":1
+		},
+		{
+			"fieldname":"item",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"reqd":0
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group",
+			"reqd":0
+		}
 	]
 }
