308bbbacbb7bc57245ff378407d39c91fb3e90b2
bobzz-zone <bobzz.zone@gmail.com>
2016-10-11 20:59:03 +0700

    last report

--- graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.js
+++ graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.js
@@ -3,6 +3,30 @@
 
 frappe.query_reports["Store Bin Cards Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd":1
+		},
+		{
+			"fieldname":"to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+			"reqd":1
+		},
+		{
+			"fieldname":"item",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+		},
+		{
+			"fieldname":"ref_no",
+			"label": __("Ref No"),
+			"fieldtype": "Data",
+		}
 	]
 }
--- graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
+++ graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
@@ -5,5 +5,25 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = ["Date:Date:200","Item:Link/Item:200","Item Name:Data:200","Receipt Ref No:Data:200","Receipt Qty:Float:100","Issues Ref No:Data:200","Issues Qty:Float:100","Balance Qty:Float:200"], []
+	item=""
+	ref_no=""
+	item2=""
+	ref_no2=""
+	if filters.get("item"):
+		item = """ and ii.item_code = '{}' """.format(filters.get("item"))
+		item2 = """ and i.item_code = '{}' """.format(filters.get("item"))
+	if filters.get("ref_no"):
+		ref_no = """ and ii.ref_no = '{}' """.format(filters.get("ref_no"))
+		ref_no2 = """ and i.ref_no = '{}' """.format(filters.get("ref_no"))
+	data = frappe.db.sql("""select pp.name , pp.date as "date , ii.item_code,ii.item_name,ii.ref_no,i.qty,"","",ii.balance_qty
+		from `tabStore Bin Card Item` ii 
+		join `tabStore Bin Card` pp on ii.parent=pp.name 
+		where pp.docstatus=1 and (pp.date between "{0}" and  "{1}"") {2} {3}
+		UNION
+		select p.name , p.date as "date" , i.item_code,i.item_name,"","",i.ref_no,i.qty,i.balance_qty
+		from `tabStore Bin Card Issue Item` i 
+		join `tabStore Bin Cards Report` p on i.parent=p.name 
+		where p.docstatus=1 and (p.date between "{0}" and  "{1}"") {4} {5} 
+		order by date""".format(filters.get("from"),filters.get("to"),item,ref_no,item2,ref_no2),as_list=1)
 	return columns, data
