77e2d39042b1ac17e2d97636fb385b8821ac829e
bobzz-zone <bobzz.zone@gmail.com>
2016-10-21 00:01:48 +0700

    report

--- graceco_tools/graceco_tools/report/stock_ledger_simplified/stock_ledger_simplified.js
+++ graceco_tools/graceco_tools/report/stock_ledger_simplified/stock_ledger_simplified.js
@@ -3,6 +3,38 @@
 
 frappe.query_reports["Stock Ledger Simplified"] = {
 	"filters": [
-
+		"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname":"warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item"
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		}
 	]
 }
