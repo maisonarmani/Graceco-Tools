97121e82ca1c60ef5709c38a991851a7d2e315e6
bobzz-zone <bobzz.zone@gmail.com>
2016-10-25 00:50:40 +0700

    payment entry

--- graceco_tools/graceco_tools/report/payment_entry_report/payment_entry_report.py
+++ graceco_tools/graceco_tools/report/payment_entry_report/payment_entry_report.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = ["Date:Date:200","Party:Data:200","Payment Type:Data:200","Amount:Currency:200","Reference Voucher"], []
-	data = frappe.db.sql("""select posting_date,party,payment_type,IF(party_type="Customer",received_amount,paid_amount),reference_no 
+	columns, data = ["Date:Date:200","Party:Data:200","Payment Type:Data:200","Amount:Currency:200","Reference Voucher:Link/Payment Entry:200"], []
+	data = frappe.db.sql("""select posting_date,party,payment_type,IF(party_type="Customer",received_amount,paid_amount),name 
 		from `tabPayment Entry` where docstatus=1 and (posting_date between "{}" and "{}") """.format(filters.get("from"),filters.get("to")),as_list=1)
 	return columns, data
