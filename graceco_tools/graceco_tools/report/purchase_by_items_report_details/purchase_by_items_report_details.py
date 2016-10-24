c4864126223e08a956b1fedbb344749da44bc19f
bobzz-zone <bobzz.zone@gmail.com>
2016-10-25 01:16:36 +0700

    a

--- graceco_tools/graceco_tools/report/purchase_by_items_report_details/purchase_by_items_report_details.py
+++ graceco_tools/graceco_tools/report/purchase_by_items_report_details/purchase_by_items_report_details.py
@@ -5,5 +5,15 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data = ["Date:Date:200","Purchase Invoice:Link/Purchase Invoice:200","Supplier:Link/Supplier:200","Qty:Float:100","UOM:Data:100","Amount:Currency:200"], []
+	supplier=""
+	if filters.get("supplier"):
+		supplier=""" and p.supplier ="{}" """.format(filters.get("supplier"))
+	data = frappe.db.sql("""select p.posting_date,p.name,p.supplier,i.qty,i.uom,i.amount
+	from `tabPurchase Invoice Item` d 
+	join `tabPurchase Invoice` p on d.parent=p.name
+	where p.docstatus=1
+	and (p.posting_time between "{}" and "{}")
+	and  d.item_code = "{}"
+	{} """.format(filters.get("from"),filters.get("to"),filters.get("item"),supplier),as_list=1)
 	return columns, data
