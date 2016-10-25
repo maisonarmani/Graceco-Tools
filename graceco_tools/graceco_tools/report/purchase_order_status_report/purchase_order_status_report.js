3df1b44bbfd44794665ee5baac253eccb8559e2d
bobzz-zone <bobzz.zone@gmail.com>
2016-10-25 21:33:50 +0700

    a

--- graceco_tools/graceco_tools/report/purchase_order_status_report/purchase_order_status_report.js
+++ graceco_tools/graceco_tools/report/purchase_order_status_report/purchase_order_status_report.js
@@ -3,6 +3,26 @@
 
 frappe.query_reports["Purchase Order Status Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd":1
+		},
+		{
+			"fieldname":"to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+			"reqd":1
+		},
+		{
+			"fieldname":"supplier",
+			"label": __("Supplier"),
+			"fieldtype": "Link",
+			"options": "Supplier",
+			"reqd":0
+		}
 	]
 }
--- graceco_tools/graceco_tools/report/purchase_order_status_report/purchase_order_status_report.py
+++ graceco_tools/graceco_tools/report/purchase_order_status_report/purchase_order_status_report.py
@@ -5,5 +5,14 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	#Date | Supplier | Item Description | Status Report | Updated By
+	columns, data = ["Date:Date:200","Purchase Order:Link/Purchase Order:200","Supplier:Link/Supplier:200","Item Name:Data:200","Status:Data:200","Updated By:Data:200"], []
+	supplier
+	if filters.get("supplier"):
+		supplier=""" and p.supplier = "{}" """.format(filters.get("supplier"))
+	data = frappe.db.sql("""select s.date,s.purchase_order,p.supplier,i.item_name,s.status,s.updated_by
+	from `tabPurchase Order Item` i 
+	join `tabPurchase Order` p on i.parent = p.name
+	join `tabPurchase Order Status` s on i.parent = s.purchase_order
+	where s.docstatus=1 and p.docstatus=1 and (s.date between "{}" and "{}") {} """.format(filters.get("from"),filters.get("to"),supplier),as_list=1)
 	return columns, data
