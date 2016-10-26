9f3962b3bdaa98e826af8b614a5ceeac66a1b50f
bobzz-zone <bobzz.zone@gmail.com>
2016-10-26 20:53:08 +0700

    tes

--- graceco_tools/graceco_tools/doctype/store_bin_card/store_bin_card.js
+++ graceco_tools/graceco_tools/doctype/store_bin_card/store_bin_card.js
@@ -1,27 +1,4 @@
 // Copyright (c) 2016, bobzz.zone@gmail.com and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Store Bin Card', {
-	refresh: function(frm) {
-		cur_frm.add_fetch("item_code","stock_uom","uom");
-		cur_frm.add_fetch("item_code","item_name","item_name");
-		cur_frm.add_fetch("item_code","description","description");
-	},
-	vaildate: function(frm) {
-    	if (frm.doc.action == "Receipt") {
-        	frm.doc.issue_item=[]
-        	validated = true;
-        	for (var i=0;i<frm.doc.receipt_item.length;i++){
-        		if (frm.doc.receipt_item[i].ref_doc!="Opening Stock" && frm.doc.receipt_item[i].ref_no=="") {
-        			validated=false;
-        			msgprint("Please enter the Reference No except for the Opening Stock");
-        		};
-        	}
-        	
-    	}else{
-        	frm.doc.receipt_item=[]
-        	validated = true;
-    	}
-	}
 
-});
