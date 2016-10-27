a25610428087b61715d264976b3354cc755f2fd6
bobby <bobzz.zone@gmail.com>
2016-10-27 12:07:45 +0000

    aa

---
+++ graceco_tools/graceco_tools/report/petty_cash_log_report/petty_cash_log_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, bobzz.zone@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Petty Cash Log Report"] = {
+	"filters": [
+
+	]
+}
