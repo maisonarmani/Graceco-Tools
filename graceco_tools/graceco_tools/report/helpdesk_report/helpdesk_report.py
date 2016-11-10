6889cad550de7793cfbc088f4009666f36a37f00
bobzz-zone <bobzz.zone@gmail.com>
2016-11-10 23:51:46 +0700

    qq

--- graceco_tools/graceco_tools/report/helpdesk_report/helpdesk_report.py
+++ graceco_tools/graceco_tools/report/helpdesk_report/helpdesk_report.py
@@ -6,7 +6,7 @@ import frappe
 
 def execute(filters=None):
 	#S/N	Date	Doc id	Request Type	Subject	Raised By	Status	Assigned to
-	columns, data = ["Date:Datetime:200","Doc id:Link/Helpdesk Ticket:200","Request Type:Link/Request Type:150","Subject:Data:200","Raised By:Link/Employee:150","Status:Data:150","Assigned to:Link/Employee:200"], []
+	columns, data = ["Date:Datetime:200","Doc id:Link/Helpdesk Ticket:200","Request Type:Link/Request Type:150","Subject:Data:200","Raised By::150","Status:Data:150","Assigned to:Link/Employee:200"], []
 	request=""
 	status=""
 	raised=""
@@ -16,7 +16,7 @@ def execute(filters=None):
 		status = """ and status = '{}' """.format(filters.get("status"))
 	if filters.get("raised"):
 		raised = """ and raised_by = '{}' """.format(filters.get("raised"))
-	data=frappe.db.sql("""select addtime(opening_date,opening_time) as "date",name,request_type,subject,raised_by,status,assigned_to
+	data=frappe.db.sql("""select addtime(opening_date,opening_time) as "date",name,request_type,subject,raised_by_name,status,assigned_to_name
 	from `tabHelpdesk Ticket`
 	where (opening_date between "{}" and "{}") {} {} {} """.format(filters.get("from"),filters.get("to"),request,status,raised),as_list=1)
 	return columns, data
