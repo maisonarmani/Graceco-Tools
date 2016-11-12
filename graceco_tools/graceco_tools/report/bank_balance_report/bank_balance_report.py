a6e4f2fc4be6fca1945e60ed2ed48ece87ba5c1b
bobzz-zone <bobzz.zone@gmail.com>
2016-11-12 21:36:22 +0700

    a

--- graceco_tools/graceco_tools/report/bank_balance_report/bank_balance_report.py
+++ graceco_tools/graceco_tools/report/bank_balance_report/bank_balance_report.py
@@ -7,5 +7,5 @@ import frappe
 def execute(filters=None):
 	columns, data = ["Bank Name Account::200","Amount:Currency:200"], []
 	data = frappe.db.sql("""select gl.account,sum(gl.debit-gl.credit) from `tabGL Entry` gl join `tabAccount` a on gl.account = a.name
-	where a.account_type = "Bank" and a.is_group=0 """)
+	where a.account_type = "Bank" and a.is_group=0  group by gl.account""")
 	return columns, data
