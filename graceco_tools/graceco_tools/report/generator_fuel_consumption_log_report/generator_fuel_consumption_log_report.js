5d99ec11dcaa546d0be8f8d6a480d5655f8aac9b
bobzz-zone <bobzz.zone@gmail.com>
2016-11-12 21:49:28 +0700

    a

--- graceco_tools/graceco_tools/report/generator_fuel_consumption_log_report/generator_fuel_consumption_log_report.js
+++ graceco_tools/graceco_tools/report/generator_fuel_consumption_log_report/generator_fuel_consumption_log_report.js
@@ -22,9 +22,9 @@ frappe.query_reports["Generator Fuel Consumption Log Report"] = {
 			"label": __("Asset"),
 			"fieldtype": "Link",
 			"options": "Asset",
-			"filters": [{
-					["Asset","asset_category","=","Plant and Machinery"] 
-				}]
+			"filters": {
+					"asset_category":"Plant and Machinery" 
+				}
 		},
 	]
 }
