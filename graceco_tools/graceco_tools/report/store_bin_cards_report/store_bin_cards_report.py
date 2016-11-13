839e1b8be460c47f623e50fbdfe770d43b988fd1
bobzz-zone <bobzz.zone@gmail.com>
2016-11-13 09:55:13 +0700

    a

--- graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
+++ graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
@@ -37,12 +37,12 @@ def execute(filters=None):
 		r,i,bb,balance=0,0,0,0
 		if rqty[row[2]]:
 			r=rqty[row[2]]
-			rqty[row[2]]+=flt(row[5])
+			rqty[row[2]]=flt(rqty[row[2]])+flt(row[5])
 		else:
 			rqty[row[2]]=flt(row[5])
 		if iqty[row[2]]:
 			i=rqty[row[2]]
-			iqty[row[2]]+=flt(row[7])
+			iqty[row[2]]=flt(iqty[row[2]])+flt(row[7])
 		else:
 			iqty[row[2]]=flt(row[7])
 		balance=i-r
