831dec1e69c47a435d40a023879c425b170af26d
bobzz-zone <bobzz.zone@gmail.com>
2016-11-13 10:08:17 +0700

    a

--- graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
+++ graceco_tools/graceco_tools/report/store_bin_cards_report/store_bin_cards_report.py
@@ -9,34 +9,39 @@ def execute(filters=None):
 	columns, data = ["Store Bin Card No:Link/Store Bin Card:200","Date:Date:200","Item:Link/Item:200","Item Name:Data:200","Opening Qty:float:200","Receipt Ref No:Data:200","Receipt Qty:Float:100","Issues Ref No:Data:200","Issues Qty:Float:100","Balance Qty:Float:200"], []
 	rqty={}
 	iqty={}
+	item=""
+	item2=""
+	if filters.get("item"):
+		item = " and ii.item_code = '{}'".format(filters.get("item"))
+		item2 = " and i.item_code = '{}'".format(filters.get("item"))
 	if filters.get("from") and filters.get("from")!="":
 		receipt_qty = frappe.db.sql("""select ii.item_code, sum(ii.qty)
 			from `tabStore Bin Card Item` ii 
 			join `tabStore Bin Card` pp on ii.parent=pp.name 
-			where pp.docstatus=1 and pp.date < "{0}" and ii.item_code = '{1}' group by ii.item_code """.format(filters.get("from"),filters.get("item")),as_list=1)
+			where pp.docstatus=1 and pp.date < "{0}" {1} group by ii.item_code """.format(filters.get("from"),item),as_list=1)
 		for row in receipt_qty:
 			rqty[row[0]]=row[1]
 		issued_qty = frappe.db.sql("""select ii.item_code,sum(ii.qty)
 			from `tabStore Bin Card Issue Item` ii 
 			join `tabStore Bin Card` pp on ii.parent=pp.name 
-			where pp.docstatus=1 and pp.date < "{0}" and ii.item_code = '{1}' group by ii.item_code """.format(filters.get("from"),filters.get("item")),as_list=1)
+			where pp.docstatus=1 and pp.date < "{0}" {1} group by ii.item_code """.format(filters.get("from"),item),as_list=1)
 		for row in issued_qty:
 			iqty[row[0]]=row[1]
 	all_data = frappe.db.sql("""select pp.name , pp.date as "date" , ii.item_code,ii.item_name,ii.ref_no,ii.qty,"",""
 		from `tabStore Bin Card Item` ii 
 		join `tabStore Bin Card` pp on ii.parent=pp.name 
-		where pp.docstatus=1 and (pp.date between "{0}" and  "{1}")  and ii.item_code = '{2}' 
+		where pp.docstatus=1 and (pp.date between "{0}" and  "{1}")  {2}
 		UNION
 		select p.name , p.date as "date" , i.item_code,i.item_name,"","",i.ref_no,i.qty
 		from `tabStore Bin Card Issue Item` i 
 		join `tabStore Bin Card` p on i.parent=p.name 
-		where p.docstatus=1 and (p.date between "{0}" and  "{1}")  and i.item_code = '{2}' 
-		order by date""".format(filters.get("from"),filters.get("to"),filters.get("item")),as_list=1)
+		where p.docstatus=1 and (p.date between "{0}" and  "{1}")  {3} 
+		order by date""".format(filters.get("from"),filters.get("to"),item,item2),as_list=1)
 	#balance = rqty-iqty
 	
 	for row in all_data:
 		r,i,bb,balance=0,0,0,0
-		frappe.throw("{}".format(rqty))
+		frappe.throw("{}".format(rqty[row[2]]))
 		if rqty[row[2]]:
 			r=flt(rqty[row[2]])
 			rqty[row[2]]=flt(rqty[row[2]])+flt(row[5])
