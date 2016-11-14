3b2b969ae5243592e7a02fc19a3b4be0cbff0944
bobzz-zone <bobzz.zone@gmail.com>
2016-11-14 17:46:09 +0700

    a

--- graceco_tools/graceco_tools/doctype/payment_voucher_form/payment_voucher_form.js
+++ graceco_tools/graceco_tools/doctype/payment_voucher_form/payment_voucher_form.js
@@ -8,19 +8,19 @@ frappe.ui.form.on('Payment Voucher Form', {
 	setup:function(frm) {
 		cur_frm.set_query("bank_account", function() {
 			return {
-				"filters": [{
+				"filters": {
 					["Account","account_type","in","Bank,Cash"],
 					["Account", "is_group","in","0"]
-				}]
+				}
 			};
 		});
 		cur_frm.set_query("expense_account", function() {
 			return {
-				"filters": [{
+				"filters": {
 					["Account","lft",">=","362"],
 					//["Account","rgt","<=","561"],
 					["Account", "is_group","in","0"]
-				}]
+				}
 			};
 		});
 
