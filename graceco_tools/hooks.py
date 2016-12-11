d04ed0812b8efb34280e39dd06547c4255a3d3c1
bobzz-zone <bobzz.zone@gmail.com>
2016-12-11 12:22:23 +0700

    aa

--- graceco_tools/hooks.py
+++ graceco_tools/hooks.py
@@ -27,7 +27,7 @@ app_license = "MIT"
 
 # application home page (will override Website Settings)
 # home_page = "login"
-fixtures = ['Custom Field','Print Format']
+fixtures = ['Custom Field','Print Format','Workflow','Workflow State','Workflow Action','Role']
 # website user home page (by Role)
 # role_home_page = {
 #	"Role": "home_page"
