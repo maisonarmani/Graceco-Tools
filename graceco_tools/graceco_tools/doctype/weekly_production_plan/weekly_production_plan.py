2f9bee93de3fa03288c38e6a9f4e63f7967c0655
bobzz-zone <bobzz.zone@gmail.com>
2016-12-18 17:39:45 +0700

    a

--- graceco_tools/graceco_tools/doctype/weekly_production_plan/weekly_production_plan.py
+++ graceco_tools/graceco_tools/doctype/weekly_production_plan/weekly_production_plan.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class WeeklyProductionPlanForm(Document):
+class WeeklyProductionPlan(Document):
 	pass
