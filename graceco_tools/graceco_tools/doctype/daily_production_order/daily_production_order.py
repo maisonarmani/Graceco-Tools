d33b749be5fb3cbdbdf3b195b956852f499fb7f8
bobzz-zone <bobzz.zone@gmail.com>
2016-12-18 17:47:47 +0700

    aa

--- graceco_tools/graceco_tools/doctype/daily_production_order/daily_production_order.py
+++ graceco_tools/graceco_tools/doctype/daily_production_order/daily_production_order.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class DailyProductionOrderForm(Document):
+class DailyProductionOrder(Document):
 	pass
